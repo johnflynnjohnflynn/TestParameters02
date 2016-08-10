89cc6989e190c9e6329a03c3e613846672365117
John Flynn <johnflynn@users.noreply.github.com>
2016-08-10 10:33:39 +0100

    Rename tests to "StateAB".

--- Source/PluginProcessorTest.cpp
+++ Source/PluginProcessorTest.cpp
@@ -27,41 +27,41 @@ void PluginProcessorTests::runTest()
     TestParameters02AudioProcessor proc;
     XmlElement xml {"TestXml"};
 
-    beginTest ("State: Store current plugin settings as a state");
+    beginTest ("StateAB: Store current plugin settings as a state");
     expectDoesNotThrow (state::saveStateToXml(proc, xml));
 
-    beginTest ("State: Check parameter was stored");
+    beginTest ("StateAB: Check parameter was stored");
     expect (xml.getDoubleAttribute ("gainID") == 0.5);
-    beginTest ("State: Check first parameter was stored");
+    beginTest ("StateAB: Check first parameter was stored");
     expect (test::equalApprox (xml.getDoubleAttribute ("gainStepID"), 0.152542));
-    beginTest ("State: Check last parameter was stored");
+    beginTest ("StateAB: Check last parameter was stored");
     expect (xml.getDoubleAttribute ("q2ID") == 0.5);
 
     xml.setAttribute("gainStepID", 0.11f);   // 0. change parameters in xml
     xml.setAttribute("gainID", 0.75f);       // 2.
     xml.setAttribute("q2ID", 0.93f);         // 7.
 
-    beginTest ("State: Load state from changed xml");
+    beginTest ("StateAB: Load state from changed xml");
     expectDoesNotThrow (state::loadStateFromXml(xml, proc));
 
-    beginTest ("State: Check first parameter was loaded");
+    beginTest ("StateAB: Check first parameter was loaded");
     expect (proc.getParameters()[0]->getValue() == 0.11f);
-    beginTest ("State: Check a parameter was loaded");
+    beginTest ("StateAB: Check a parameter was loaded");
     expect (proc.getParameters()[2]->getValue() == 0.75f);
-    beginTest ("State: Check last parameter was loaded");
+    beginTest ("StateAB: Check last parameter was loaded");
     expect (proc.getParameters()[7]->getValue() == 0.93f);
 
-    beginTest ("State: Toggle AB state");
+    beginTest ("StateAB: Toggle AB state");
     expectDoesNotThrow(proc.stateAB.toggleAB());
-    beginTest ("State: Check parameter is same");
+    beginTest ("StateAB: Check parameter is same");
     expect (proc.getParameters()[0]->getValue() == 0.11f);
 
-    beginTest ("State: Change parameter");
+    beginTest ("StateAB: Change parameter");
     expectDoesNotThrow(proc.getParameters()[0]->setValueNotifyingHost(0.2));
-    beginTest ("State: Toggle AB state and check");
+    beginTest ("StateAB: Toggle AB state and check");
     expectDoesNotThrow(proc.stateAB.toggleAB());
     expect (proc.getParameters()[0]->getValue() == 0.11f);
-    beginTest ("State: Toggle AB state and check");
+    beginTest ("StateAB: Toggle AB state and check");
     expectDoesNotThrow(proc.stateAB.toggleAB());
     expect (proc.getParameters()[0]->getValue() == 0.2f);
 }
